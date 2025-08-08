8e804f49c19cf991b85e760b5bb8354e64f00fc5
João Vitor Avelar <jvitoravelar1200@gmail.com>
2025-08-08 14:05:53 -0300

    criando o Service

--- app/src/main/java/com/vitoravelar/pokedex/service/remote/PokeService.kt
+++ app/src/main/java/com/vitoravelar/pokedex/service/remote/PokeService.kt
@@ -1,11 +1,32 @@
 package com.vitoravelar.pokedex.service.remote
 
+import com.vitoravelar.pokedex.feature.model.Pokemon
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 
 interface PokeService {
-    @GET("")
+
+    @GET("/pokemon")
     @Headers("Content-type: application/json")
-    suspend fun getAllPokemon()
+    suspend fun getAllPokemons(): Response<List<Pokemon>>
+
+    @GET("/pokemon/{id}")
+    @Headers("Content-type: application/json")
+    suspend fun getPokemonById(@Path(value = "id") id: Int): Response<Pokemon>
+
+    @GET("/pokemon/{name}")
+    @Headers("Content-type: application/json")
+    suspend fun getPokemonByName(@Path(value = "name") name: String): Response<Pokemon>
+
+    @GET("/type}")
+    @Headers("Content-type: application/json")
+    suspend fun getTypePokemon() // : Response<Type>
+
+    @GET("/ability}")
+    @Headers("Content-type: application/json")
+    suspend fun getAbility() // : Response<Ability>
+
 
 }

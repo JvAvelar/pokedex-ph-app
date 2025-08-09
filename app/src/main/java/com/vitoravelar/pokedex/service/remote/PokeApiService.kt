1bafda3ecdb0e5a32c7c73cbf6fbc36306b62210
João Vitor Avelar <jvitoravelar1200@gmail.com>
2025-08-08 22:01:03 -0300

    Ajustando os retornos com os models

--- app/src/main/java/com/vitoravelar/pokedex/service/remote/PokeApiService.kt
+++ app/src/main/java/com/vitoravelar/pokedex/service/remote/PokeApiService.kt
@@ -1,33 +1,37 @@
 package com.vitoravelar.pokedex.service.remote
 
+import com.vitoravelar.pokedex.feature.model.AbilityResponse
+import com.vitoravelar.pokedex.feature.model.PokemonDetail
 import com.vitoravelar.pokedex.feature.model.PokemonResponse
+import com.vitoravelar.pokedex.feature.model.TypeResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-interface PokeService {
+interface PokeApiService {
 
     @GET("pokemon")
     @Headers("Content-type: application/json")
     suspend fun getAllPokemons(
         @Query("limit") limit: Int = 20,
         @Query("offset") offset: Int = 0
-    ): Response<List<PokemonResponse>>
+    ): Response<PokemonResponse>
 
     @GET("pokemon/{idOrName}")
     @Headers("Content-type: application/json")
-    suspend fun getPokemonById(@Path(value = "idOrName") idOrName: Int): Response<PokemonResponse> // Response<PokemonDetail>
-
+    suspend fun getPokemonById(
+        @Path(value = "idOrName") idOrName: Int
+    ): Response<PokemonDetail>
 
     @GET("type}")
     @Headers("Content-type: application/json")
-    suspend fun getTypePokemon() // : Response<Type>
+    suspend fun getTypePokemon(): Response<TypeResponse>
 
     @GET("ability}")
     @Headers("Content-type: application/json")
-    suspend fun getAbility() // : Response<Ability>
+    suspend fun getAbility(): Response<AbilityResponse>
 
 
 }
